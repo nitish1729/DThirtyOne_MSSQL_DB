4607e8af0e9f1f22badf43a0da11338ce43efd59
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-22 22:51:11 +0530

    sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+use BridgeLabz;
+create table( 
+id varchar (5)
+batch varchar(5), 
+name varchar(100),
+DOB date,
+gender varchar(5),
+
+Joining_date date,
+phone_Number varchar(12),
+email_Id varchar(50),
+teacher_name varchar(100),
+topic varchar(100),
+Address text
+
+)
